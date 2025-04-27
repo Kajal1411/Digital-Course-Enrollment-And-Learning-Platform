8f81888f0305b3ed2ad7469c544fd7373c7128b0
Kajal Pawar <kp668138@gmail.com>
2025-04-28 00:10:10 +0530

    Add project all files

---
+++ script.js
@@ -0,0 +1,71 @@
+    //for FAQs answers
+    
+    var que1=document.getElementById('que1');
+    var Ans1=document.getElementById('Ans1');
+    que1.addEventListener('click',function()
+    {
+        if (Ans1.style.display === 'block') 
+        {
+            Ans1.style.display = 'none';
+        } 
+        else 
+        {
+            Ans1.style.display = 'block';
+        }
+    });
+
+    var que2=document.getElementById('que2');
+    var Ans2=document.getElementById('Ans2');
+    que2.addEventListener('click',function()
+    {
+        if (Ans2.style.display === 'block') 
+        {
+            Ans2.style.display = 'none';
+        } 
+        else 
+        {
+            Ans2.style.display = 'block';
+        }
+    });
+
+    var que3=document.getElementById('que3');
+    var Ans3=document.getElementById('Ans3');
+    que3.addEventListener('click',function()
+    {
+        if (Ans3.style.display === 'block') 
+        {
+            Ans3.style.display = 'none';
+        } 
+        else 
+        {
+            Ans3.style.display = 'block';
+        }
+    });
+
+    var que4=document.getElementById('que4');
+    var Ans4=document.getElementById('Ans4');
+    que4.addEventListener('click',function()
+    {
+        if (Ans4.style.display === 'block') 
+        {
+            Ans4.style.display = 'none';
+        } 
+        else 
+        {
+            Ans4.style.display = 'block';
+        }
+    });
+
+    var que5=document.getElementById('que5');
+    var Ans5=document.getElementById('Ans5');
+    que5.addEventListener('click',function()
+    {
+        if (Ans5.style.display === 'block') 
+        {
+            Ans5.style.display = 'none';
+        } 
+        else 
+        {
+            Ans5.style.display = 'block';
+        }
+    });
